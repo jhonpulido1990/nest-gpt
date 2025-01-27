e0c9fcc0dc55d7b4d770592510f24b5c8d305b19
jhonpulido1990 <3825@holbertonstudents.com>
2025-01-27 17:30:34 -0500

    nuestra primera prompt

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+
 import { orthographyCheckCase } from './use-cases';
 import { OrthographyDto } from './dtos/orthography.dto';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   // solo llama casos de uso
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckCase({
+    return await orthographyCheckCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
   }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,11 +1,30 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckCase = async (options: Options) => {
+export const orthographyCheckCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: 'eres un asistente muy util.',
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    model: 'gpt-4o-mini',
+  });
+
+  console.log(completion, prompt);
+
+  return completion.choices[0];
 };
