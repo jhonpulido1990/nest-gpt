a8b530a2d641cf5c86c8b851bc5edb216f0f8963
jhonpulido1990 <3825@holbertonstudents.com>
2025-02-11 15:20:02 -0500

    obtener la imagen basado en el prompt

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -10,5 +10,21 @@ export const imageGenerationUseCase = async (
   openai: OpenAI,
   { prompt, originalImage, maskImage }: Options,
 ) => {
-  console.log({ prompt, originalImage, maskImage });
+  // Todo: verificar original image
+
+  const response = await openai.images.generate({
+    prompt: prompt,
+    model: 'dall-e-2',
+    n: 1,
+    size: '1024x1024',
+    quality: 'standard',
+    response_format: 'url',
+  });
+
+  console.log(response);
+  return {
+    url: response.data[0].url,
+    localPath: '',
+    revised_prompt: response.data[0].revised_prompt,
+  };
 };
