75db9e1a1f6b70060151e4458491dfd2660582f7
jhonpulido1990 <3825@holbertonstudents.com>
2025-02-11 17:47:55 -0500

    editar imagen

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,7 @@
 import OpenAI from 'openai';
-import { downloadImageAsPng } from 'src/helpers';
+import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
+import * as fs from 'fs';
+import path from 'path';
 
 interface Options {
   prompt: string;
@@ -13,22 +15,48 @@ export const imageGenerationUseCase = async (
 ) => {
   // Todo: verificar original image
 
-  const response = await openai.images.generate({
-    prompt: prompt,
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      prompt: prompt,
+      model: 'dall-e-2',
+      n: 1,
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
+
+    // Todo: guardar imagen en FS
+
+    const url = await downloadImageAsPng(response.data[0].url);
+
+    return {
+      url: url,
+      openAIUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
+    };
+  }
+
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = openai.images.edit({
     model: 'dall-e-2',
+    prompt: prompt,
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
-    quality: 'standard',
     response_format: 'url',
   });
 
-  // Todo: guardar imagen en FS
+  const localImagePath = await downloadImageAsPng((await response).data[0].url);
+  const fileName = path.basename(localImagePath);
 
-  const url = await downloadImageAsPng(response.data[0].url);
+  const publicUrl = `localhost:300/${fileName}`;
 
   return {
-    url: url,
-    openAIUrl: response.data[0].url,
-    revised_prompt: response.data[0].revised_prompt,
+    url: publicUrl,
+    openAIUrl: (await response).data[0].url,
+    revised_prompt: (await response).data[0].revised_prompt,
   };
 };
